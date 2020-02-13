98d0534f3216d8edb8deaa9d32fb35d99c5e5ba0
Brian Yu <brian@cs50.harvard.edu>
2020-02-13 10:10:50 -0500

    Update valid flight check

--- flights/models.py
+++ flights/models.py
@@ -18,7 +18,7 @@ class Flight(models.Model):
         return f"{self.id} : {self.origin} to {self.destination}"
 
     def is_valid_flight(self):
-        return self.origin != self.destination and self.duration >= 0
+        return self.origin != self.destination or self.duration >= 0
 
 
 class Passenger(models.Model):
